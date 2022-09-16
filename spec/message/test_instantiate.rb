4aa1cf624d9582e066b61f79a8a3eb112d7975b9
Bernard Lambeau <blambeau@gmail.com>
2022-09-16 17:05:25 +0200

    Add tests showing that we don't enable magic markdown links.
    It might be a security issue to do it, so these tests are here to
    stay and avoid regression.

--- spec/message/test_instantiate.rb
+++ spec/message/test_instantiate.rb
@@ -6,12 +6,24 @@ module Talktome
       Message.new(path).instantiate(data)
     }
 
-    let(:path) { Path.dir/'with-frontmatter.md' }
-    let(:data) { { url: "http://foo", who: "bar"} }
+    describe "base contract" do
+      let(:path) { Path.dir/'with-frontmatter.md' }
+      let(:data) { { url: "http://foo", who: "bar"} }
 
-    it 'works as expected' do
-      expect(subject.template_content).to eql("Subject: Hello bar\n\nHello bar [test](http://foo)\n")
-      expect(subject.metadata).to eql("hello" => "World", "subject" => "Hello bar")
+      it 'works as expected' do
+        expect(subject.template_content).to eql("Subject: Hello bar\n\nHello bar [test](http://foo)\n")
+        expect(subject.metadata).to eql("hello" => "World", "subject" => "Hello bar")
+      end
+    end
+
+    describe "security" do
+      let(:path) { Path.dir/'security.md' }
+      let(:data) { { url: "http://foo" } }
+
+      it "doesn't have magic links" do
+        expect(subject.template_content).to eql("Hello http://foo\n")
+        expect(subject.to_html).to eql("<p>Hello http://foo</p>\n")
+      end
     end
 
   end
