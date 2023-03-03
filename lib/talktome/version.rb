e8ddc907c558d1cfb365e1893cbc1af5c9a2a1a5
Louis Lambeau <llambeau@enspirit.be>
2023-03-03 15:43:16 +0100

    Bump to 2.1.0 and release.

--- lib/talktome/version.rb
+++ lib/talktome/version.rb
@@ -1,8 +1,8 @@
 module Talktome
   module Version
     MAJOR = 2
-    MINOR = 0
-    TINY  = 4
+    MINOR = 1
+    TINY  = 0
   end
   VERSION = "#{Version::MAJOR}.#{Version::MINOR}.#{Version::TINY}"
 end
